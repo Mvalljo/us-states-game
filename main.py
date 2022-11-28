fbf369dfbfbd580217dba7cd6c36db53b8e983c7
Snorlax <maria.vallejo29@yahoo.com>
2022-11-28 17:12:43 -0600

    ask for user input

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+print(answer_state)
 
 screen.exitonclick()
