0b31081e09730a100cc3657a324aa6be84d73a37
Snorlax <maria.vallejo29@yahoo.com>
2022-11-29 17:02:29 -0600

    guess state and add to map

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,7 +7,35 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-print(answer_state)
+correct_guesses_list = []
 
-screen.exitonclick()
+data = pandas.read_csv("50_states.csv")
+state_list = data["state"].to_list()
+
+while len(correct_guesses_list) != 50:
+    correct_guesses_num = str(len(correct_guesses_list))
+    answer_state = screen.textinput(title=correct_guesses_num + "/50 States Correct",
+                                    prompt="What's another state's name?").title()
+
+    if answer_state == "Exit":
+        break
+
+    if answer_state in state_list:
+        s = turtle.Turtle()
+        s.penup()
+        s.hideturtle()
+        state_data = data[data.state == answer_state]
+        s.goto(int(state_data.x), int(state_data.y))
+        s.write(answer_state)
+        if answer_state not in correct_guesses_list:
+            correct_guesses_list.append(answer_state)
+
+# # state_to_learn
+# print(data)
+# need_to_learn = []
+# for num in range(50):
+#     if correct_guesses_list[num] in data["state"][num]:
+#         data.
+#         print("hi")
+
+# print(data)
